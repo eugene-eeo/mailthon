c8b38c2fa46d54db20526074cbccdb2cce537376
Eugene Eeo <packwolf58@gmail.com>
2015-05-21 16:00:50 +0800

    refactor respone tests

--- tests/test_response.py
+++ tests/test_response.py
@@ -2,30 +2,34 @@ import pytest
 from mailthon.response import Response, SendmailResponse
 
 
+OK_RES = (250, 'm')
+NO_RES = (100, 'm')
+
+
 class TestResponse:
     def test_response_ok(self):
-        assert Response((250, 'm')).ok
-        assert not Response((100, 'm')).ok
+        assert Response(OK_RES).ok
+        assert not Response(NO_RES).ok
 
     def test_response_attrs(self):
-        r = Response((250, 'm'))
+        r = Response(OK_RES)
         assert r.status_code == 250
         assert r.message == 'm'
 
 
 class TestSendmailResponse:
     def test_response_ok(self):
-        assert SendmailResponse((250, 'm'), {}).ok
-        assert not SendmailResponse((250, 'm'), {'k': (255, 'm')}).ok
-        assert not SendmailResponse((255, 'm'), {}).ok
+        assert SendmailResponse(OK_RES, {}).ok
+        assert not SendmailResponse(OK_RES, {'k': NO_RES}).ok
+        assert not SendmailResponse(NO_RES, {}).ok
 
     def test_response_rejected(self):
-        res = SendmailResponse((250, 'm'), {
-            'fail1': (255, 'reason'),
-            'fail2': (255, 'reason'),
+        res = SendmailResponse(OK_RES, {
+            'fail1': NO_RES,
+            'fail2': NO_RES,
         })
         for addr, r in res.rejected.items():
             assert addr in ('fail1', 'fail2')
-            assert r.status_code == 255
-            assert r.message == 'reason'
+            assert r.status_code ==  100
+            assert r.message == 'm'
             assert not r.ok
