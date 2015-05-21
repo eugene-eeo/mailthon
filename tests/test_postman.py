7d349c8ae6cc5d3e52a1a1d60ef3e6721c2f763b
Eugene Eeo <packwolf58@gmail.com>
2015-05-21 15:35:26 +0800

    test more postman

--- tests/test_postman.py
+++ tests/test_postman.py
@@ -1,12 +1,17 @@
 import pytest
 from mailthon.api import html
+from mailthon.middleware import Auth
 from .mocks import MyPostman, MockedSMTP
 
 
 class TestPostman:
     @pytest.fixture
     def postman(self):
-        return MyPostman('smtp.me.com', 587)
+        return MyPostman(
+            server='smtp.me.com',
+            port=587,
+            middleware=[Auth('user', 'pass')],
+            )
 
     @pytest.fixture
     def envelope(self):
@@ -18,12 +23,25 @@ class TestPostman:
             encoding='ascii',
         )
 
+    def match_vcr(self, smtp, envelope):
+        sendmail_args = (envelope.sender,
+                         envelope.receivers)
+        assert smtp.vcr == [
+            ('ehlo', ()),
+            ('login', ('user', 'pass')),
+            ('sendmail', sendmail_args),
+            ('noop', ()),
+            ('quit', ()),
+        ]
+        assert smtp.closed
+
     def test_connection(self, postman):
         with postman.connection() as conn:
             assert conn.server == 'smtp.me.com'
             assert conn.port == 587
         assert conn.vcr == [
             ('ehlo', ()),
+            ('login', ('user', 'pass')),
             ('quit', ()),
         ]
 
@@ -31,15 +49,11 @@ class TestPostman:
         r = postman.send(envelope)
         assert r.ok
 
-        sendmail_args = (envelope.sender,
-                         envelope.receivers)
+        self.match_vcr(MockedSMTP.instance, envelope)
 
-        smtp = MockedSMTP.instance
-        actions = smtp.vcr
-        assert actions == [
-            ('ehlo', ()),
-            ('sendmail', sendmail_args),
-            ('noop', ()),
-            ('quit', ()),
-        ]
-        assert smtp.closed
+    def test_deliver(self, postman, envelope):
+        with postman.connection() as conn:
+            response = postman.deliver(conn, envelope)
+            assert response.ok
+
+        self.match_vcr(conn, envelope)
