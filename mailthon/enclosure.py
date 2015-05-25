0fb3a9542262f6e7fd4f896028a209e3b3c8b7f3
Eugene Eeo <packwolf58@gmail.com>
2015-05-25 19:16:23 +0800

    document and refactor enclosures

--- mailthon/enclosure.py
+++ mailthon/enclosure.py
@@ -1,3 +1,14 @@
+"""
+    mailthon.enclosure
+    ~~~~~~~~~~~~~~~~~~
+
+    Implements Enclosure objects- parts that collectively
+    make up body of the email.
+
+    :copyright: (c) 2015 by Eeo Jun
+    :license: MIT, see LICENSE for details
+"""
+
 from email.encoders import encode_base64
 from email.message import Message
 from email.mime.base import MIMEBase
@@ -8,19 +19,47 @@ from .headers import Headers, content_disposition
 
 
 class Enclosure(object):
+    """
+    Base class for Enclosure objects to inherit from.
+    An enclosure is a part of the enclosure in a real
+    envelope- it contains part of the content to be
+    sent.
+
+    :param headers: Iterable of headers to include,
+        stored in an RFC-compliant Headers mapping
+        internally under the headers attribute.
+    """
+
     def __init__(self, headers=()):
         self.headers = Headers(headers)
 
-    def prepare_mime(self):
+    def mime_object(self):
+        """
+        To be overriden. Returns the generated MIME
+        object, without applying the internal headers.
+        """
         raise NotImplementedError
 
     def mime(self):
-        mime = self.prepare_mime()
+        """
+        Returns the finalised mime object, after
+        applying the internal headers.
+        """
+        mime = self.mime_object()
         self.headers.prepare(mime)
         return mime
 
 
 class PlainText(Enclosure):
+    """
+    Enclosure that has a text/plain mimetype.
+
+    :param content: Unicode or bytes string.
+    :param encoding: Encoding used to serialize the
+        content or the encoding of the content.
+    :param headers: Optional headers.
+    """
+
     subtype = 'plain'
 
     def __init__(self, content, encoding='utf-8', **kwargs):
@@ -28,17 +67,36 @@ class PlainText(Enclosure):
         self.content = content
         self.encoding = encoding
 
-    def prepare_mime(self):
+    def mime_object(self):
         return MIMEText(self.content,
                         self.subtype,
                         self.encoding)
 
 
 class HTML(PlainText):
+    """
+    Subclass of PlainText with a text/html mimetype.
+    """
+
     subtype = 'html'
 
 
 class Binary(Enclosure):
+    """
+    An Enclosure subclass for binary content. If the
+    content is HTML or any kind of plain-text then
+    the HTML or PlainText Enclosures are receommended
+    since they have a simpler interface.
+
+    :param content: A bytes string.
+    :param mimetype: Mimetype of the content.
+    :param encoding: Optional encoding of the content.
+    :param encoder: An optional encoder_ function.
+    :param headers: Optional headers.
+
+    .. _encoder: https://docs.python.org/2/library/email.encoders.html
+    """
+
     def __init__(self, content, mimetype, encoding=None,
                  encoder=encode_base64, **kwargs):
         Enclosure.__init__(self, **kwargs)
@@ -47,7 +105,7 @@ class Binary(Enclosure):
         self.encoding = encoding
         self.encoder = encoder
 
-    def prepare_mime(self):
+    def mime_object(self):
         mime = MIMEBase(*self.mimetype.split('/'))
         mime.set_payload(self.content)
         if self.encoding:
@@ -60,6 +118,18 @@ class Binary(Enclosure):
 
 
 class Attachment(Binary):
+    """
+    Binary subclass for easier file attachments.
+    Basically using this class has the advantage
+    that fetching the file contents is lazy, which
+    may be desired. Else use the Binary class. Also,
+    the Content-Disposition header is automatically
+    set.
+
+    :param path: Absolute/Relative path to the file.
+    :param headers: Optional headers.
+    """
+
     def __init__(self, path, headers=()):
         self.path = path
         self.mimetype, self.encoding = guess(path)
@@ -70,5 +140,8 @@ class Attachment(Binary):
 
     @property
     def content(self):
+        """
+        Lazily returns the bytes contents of the file.
+        """
         with open(self.path, 'rb') as handle:
             return handle.read()
