b3324e94ff5972e3cabe93994262acfc3935de17
Eugene Eeo <packwolf58@gmail.com>
2015-05-25 10:23:31 +0800

    document and make examples work

--- mailthon/envelope.py
+++ mailthon/envelope.py
@@ -3,19 +3,41 @@ from .headers import Headers
 
 
 class Envelope(object):
+    """
+    Encapsulates the concept of an Envelope- there
+    can be multiple stamps (*headers*) and multiple
+    "things" inside the *enclosure*.
+
+    :param headers: An iterable/mapping of headers.
+    :param enclosure: A list of enclosure objects.
+    """
+
     def __init__(self, headers, enclosure):
         self.headers = Headers(headers)
         self.enclosure = enclosure
 
     @property
     def sender(self):
+        """
+        Returns the sender of the envelope, obtained
+        from the headers.
+        """
         return self.headers.sender
 
     @property
     def receivers(self):
+        """
+        Returns a list of receiver addresses.
+        """
         return self.headers.receivers
 
     def mime(self):
+        """
+        Returns a mime object. Internally this
+        generates a ``MIMEMultipart`` object and
+        prepares it using the internal headers
+        object.
+        """
         mime = MIMEMultipart()
         for item in self.enclosure:
             mime.attach(item.mime())
@@ -23,4 +45,9 @@ class Envelope(object):
         return mime
 
     def string(self):
+        """
+        Returns the MIME object as a string-
+        i.e., calls the ``as_string`` method of
+        the generated MIME object.
+        """
         return self.mime().as_string()
