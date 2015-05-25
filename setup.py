23ac2d715f6529dc7864b57b2de85ceb39573c30
Eugene Eeo <packwolf58@gmail.com>
2015-05-25 11:29:26 +0800

    switch to setup.py for tests

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--strict', '--verbose', '--tb=long', 'tests']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 
 setup(
@@ -25,4 +38,7 @@ setup(
     package_data={'mailthon': ['LICENSE', 'README.rst']},
     packages=['mailthon'],
     tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    platforms='any',
+    zip_safe=False,
 )
