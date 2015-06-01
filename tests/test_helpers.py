e88e16b6d7a75743968a639310b29638bb82e0e3
Eugene Eeo <packwolf58@gmail.com>
2015-06-01 12:58:39 +0800

    test encode_address

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,6 @@
+# coding=utf8
 import pytest
-from mailthon.helpers import guess, format_addresses
+from mailthon.helpers import guess, format_addresses, encode_address
 
 
 def test_guess_recognised():
@@ -18,3 +19,9 @@ def test_format_addresses():
         'Fender <fender@mail.com>',
     ])
     assert chunks == 'Sender <sender@mail.com>, Fender <fender@mail.com>'
+
+
+def test_encode_address():
+    assert encode_address(u'mail@mail.com') == b'mail@mail.com'
+    assert encode_address(u'mail@måil.com') == b'mail@xn--mil-ula.com'
+    assert encode_address(u'måil@måil.com') == b'm\xc3\xa5il@xn--mil-ula.com'
