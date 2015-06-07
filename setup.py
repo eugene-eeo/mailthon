c8733fcba0197680f9a9f203e45edf790d2dda03
Eugene Eeo <packwolf58@gmail.com>
2015-06-07 14:39:00 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class PyTest(TestCommand):
 
 setup(
     name='mailthon',
-    version='0.0.1',
+    version='0.1.0',
     description='Elegant email library',
     long_description=open('README.rst', 'rb').read().decode('utf8'),
     author='Eeo Jun',
