c7c43f85e75df367de8d1a1b6b6c11cf981594b0
Eugene Eeo <packwolf58@gmail.com>
2015-06-08 21:28:50 +0800

    add docstrings [skip ci]

--- mailthon/envelope.py
+++ mailthon/envelope.py
@@ -11,9 +11,10 @@
 
 class Envelope(object):
     """
-    Encapsulates the concept of an Envelope- a wrapper
-    around some content in the form of an *enclosure*,
-    and dealing with SMTP specific idiosyncracies.
+    Enclosure adapter for encapsulating the concept of
+    an Envelope- a wrapper around some content in the
+    form of an *enclosure*, and dealing with SMTP
+    specific idiosyncracies.
 
     :param enclosure: An enclosure object to wrap around.
     :param mail_from: The "real" sender. May be omitted.
@@ -28,14 +29,31 @@ class Envelope(object):
 
     @property
     def sender(self):
+        """
+        Returns the real sender if set in the *mail_from*
+        parameter/attribute, else returns the sender
+        attribute from the wrapped enclosure.
+        """
         return self.mail_from or self.enclosure.sender
 
     @property
     def receivers(self):
+        """
+        Returns the "real" receivers which will be passed
+        to the ``RCPT TO`` command (in SMTP) if specified
+        in the *rcpt_to* attribute/parameter. Else, return
+        the receivers attribute from the wrapped enclosure.
+        """
         return self.rcpt_to or self.enclosure.receivers
 
     def mime(self):
+        """
+        Returns the mime object from the enclosure.
+        """
         return self.enclosure.mime()
 
     def string(self):
+        """
+        Returns the stringified mime object.
+        """
         return self.enclosure.string()
